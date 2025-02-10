714e4c491d6ee46dbd17fbf1aada9dbc781a84c6
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-10 15:47:09 +0530

    productservice added

--- Day9codeshare.cs
+++ Day9codeshare.cs
@@ -981,5 +981,35 @@ namespace MyRazorApp.Models
 }
 
 
+In the project add then in Pages fodler add one folder with name service   and in that add one class with the name ProductService
+same as above and copy the internal code means inside class copy code 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRazorApp.Pages.service
+{
+    public class ProductService
+    {
+        private static List<Product> products = new List<Product>
+     {
+         new Product { Id = 1, Name = "Product 1", Price = 10.0m },
+         new Product { Id = 2, Name = "Product 2", Price = 20.0m },
+         new Product { Id = 3, Name = "Product 3", Price = 30.0m },
+     };
+
+        public List<Product> GetAll()
+        {
+            // Return the list of all products
+            return products;
+        }
+
+
+
+
+    }
+}
+	
 
 
